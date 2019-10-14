445e01175043fc0aa2de938852db582366d3d40b
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 13:46:35 -0400

    Updates

---
+++ precache-manifest.b4e84494db0ecd9d6ebdffe4d23a5b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c7f31610c60ba5079838e220713773",
+    "url": "/slow_cookers/index.html"
+  },
+  {
+    "revision": "8e958ddb0a5cbbda2e1b",
+    "url": "/slow_cookers/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/slow_cookers/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "8e958ddb0a5cbbda2e1b",
+    "url": "/slow_cookers/static/js/main.5f8d4543.chunk.js"
+  },
+  {
+    "revision": "77290fdf26dfae9e6716",
+    "url": "/slow_cookers/static/js/runtime~main.ccd4388c.js"
+  }
+]);
